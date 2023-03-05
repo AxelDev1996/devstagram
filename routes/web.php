b979cfe44e3d9832578297257d1e14def1c675fd
BlackBit-AI <71159890+BlackBit-AI@users.noreply.github.com>
2023-03-05 06:12:06 -0300

    new file:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	modified:   resources/css/app.css 	new file:   resources/views/layouts/app.blade.php 	new file:   resources/views/nosotros.blade.php 	new file:   resources/views/principal.blade.php 	new file:   resources/views/tienda.blade.php 	deleted:    resources/views/welcome.blade.php 	modified:   routes/web.php 	new file:   tailwind.config.js

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('principal');
+});
+Route::get('/tienda-virtual', function () {
+    return view('tienda');
+});
+Route::get('/nosotros', function () {
+    return view('nosotros');
 });
