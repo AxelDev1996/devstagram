b14398f444aaa437aafe38f2c2d358cb1fbaa1c5
BlackBit-AI <71159890+BlackBit-AI@users.noreply.github.com>
2023-03-05 21:37:04 -0300

    new file:   app/Http/Controllers/RegisterController.php 	new file:   public/img/login.jpg 	new file:   public/img/registrar.jpg 	new file:   resources/views/auth/register.blade.php 	modified:   resources/views/layouts/app.blade.php 	deleted:    resources/views/nosotros.blade.php 	deleted:    resources/views/tienda.blade.php 	modified:   routes/web.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,28 @@
 
         <title>DevStagram - @yield('titulo')</title>
         @vite('resources/css/app.css')
-
     </head>
-    <body class="antialiased">
+    <body class="antialiased bg-gray-100">
+        <header class="p-5 border-b bg-white shadow">
+            <div class="container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">DevStagram</h1>
 
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="">Login</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="">Crear Cuenta</a>
+                </nav>
+            </div>
+        </header>
 
+        <main class="container mx-auto mt-16">
+            <h2 class="font-black text-center text-3xl mb-10">
+                @yield('titulo')
+            </h2>
+            @yield('contenido')
+        </main>
+
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
+            Devstagram - Todos los derechos reservados. {{now()->year;}}
+        </footer>
     </body>
 </html>
