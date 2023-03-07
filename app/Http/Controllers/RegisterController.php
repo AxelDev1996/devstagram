e1e1b90588dd8d05e9e7a3b1aa5d39d272f84a28
BlackBit-AI <71159890+BlackBit-AI@users.noreply.github.com>
2023-03-07 04:30:36 -0300

    modified:   app/Http/Controllers/RegisterController.php 	modified:   config/app.php 	new file:   database/migrations/2023_03_07_070853_add_username_to_users_table.php 	new file:   resources/lang/es 	modified:   resources/views/auth/register.blade.php 	modified:   resources/views/layouts/app.blade.php 	modified:   routes/web.php

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,7 +11,19 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store(){
-        dd('Post...');
+    public function store(Request $request){
+        //dd($request);
+        // dd($request->get('username'));
+
+
+        //Validacion.
+        $this->validate($request, [
+            'name' => 'required|min:2|max:30|alpha_num',
+            'username' => 'required|unique:users|min:3|max:20|alpha_num',
+            'email' => 'required|email|unique:users|max:60',
+            'password' => 'required|confirmed|min:6|alpha_num'
+        ]);
+
+        dd('creando usuario..');
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app');
+@extends('layouts.app')
 
 
 @section('titulo')
@@ -11,24 +11,63 @@
             <img src="{{ asset('img/registrar.jpg') }}" alt="Imagen Registro de Usuarios">
         </div>
 
-
         <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" >
-            <form action="/crear-cuenta" method="POST">
+            <form action="{{route('register');}}" method="POST">
+                @csrf
                 <div class="mb-5">
                     <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">Nombre</label>
-                    <input id="name" name="name" type="text" placeholder="Tu Nombre" class="border p-3 w-full rounded-lg">
+                    <input
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="Tu Nombre"
+                        class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
+                        value="{{ old('name') }}"
+                    />
+                    @error('name')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">Nombre De Usuario</label>
-                    <input id="username" name="username" type="text" placeholder="Tu Nombre De Usuario" class="border p-3 w-full rounded-lg">
+                    <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Tu Nombre De Usuario"
+                        class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
+                        value="{{ old('username') }}"
+                    />
+                    @error('username')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">Email</label>
-                    <input id="email" name="email" type="email" placeholder="Tu Correo Electronico" class="border p-3 w-full rounded-lg">
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Tu Correo Electronico"
+                        class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror"
+                        value="{{ old('email') }}"
+                    />
+                    @error('email')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">Password</label>
-                    <input id="password" name="password" type="password" placeholder="La contraseña de tu cuenta" class="border p-3 w-full rounded-lg">
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="La contraseña de tu cuenta"
+                        class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror"
+                    />
+                    @error('password')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">Repetir Password</label>
@@ -40,6 +79,8 @@
                     value="Crear Cuenta"
                     class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
                 />
+
+
             </form>
         </div>
     </div>
