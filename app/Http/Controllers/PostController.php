3ade3b540ca535a9032724f40cd524817e043a7d
BlackBit-AI <71159890+BlackBit-AI@users.noreply.github.com>
2023-03-12 11:25:07 -0300

    modified:   app/Http/Controllers/PostController.php 	modified:   app/Models/Post.php 	modified:   app/Models/User.php 	new file:   public/uploads/0831a8fb-14ad-449e-a94f-cf2d0669e597.jpg 	new file:   public/uploads/42df6db9-5b5c-45f8-97d1-9b947b437980.png 	new file:   public/uploads/8659d14e-4b3e-4102-bc60-b72dcb297571.png 	new file:   public/uploads/93584ccf-850d-40e7-b645-99c0316e5dfa.jpg 	new file:   public/uploads/d4c820e8-9db6-4fbc-a58e-b780e1088c6c.png

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -16,8 +17,14 @@ class PostController extends Controller
 
     //
     public function index(User $user){
+
+        $posts = Post::where('user_id', $user->id)->get();
+
+        dd($posts);
+
         return view('dashboard', [
-            'user' => $user
+            'user' => $user,
+            'posts' => $posts
         ]);
     }
 
@@ -32,5 +39,33 @@ class PostController extends Controller
             'descripcion' => 'required',
             'imagen' => 'required'
         ]);
+
+        // Post::create([
+        //     'titulo' => $request->titulo,
+        //     'descripcion' => $request->descripcion,
+        //     'imagen' => $request->imagen,
+        //     'user_id' => auth()->user()->id
+        // ]);
+
+        // //Otra forma de crear registros.
+        // $post = new Post;
+
+        // $post->titulo = $request->titulo;
+        // $post->descripcion = $request->descripcion;
+        // $post->imagen = $request->imagen;
+        // $post->user_id = auth()->user()->id;
+
+
+        // $post->save();
+
+        $request->user()->posts()->create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id
+        ]);
+
+
+        return redirect()->route('post.index', auth()->user()->username);
     }
 }
